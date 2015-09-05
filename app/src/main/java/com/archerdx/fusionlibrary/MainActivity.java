c9ba7434c841778e62338fa139866b5b7dd3dbf3
Braceface <stachick@gmail.com>
2015-09-05 15:18:35 -0600

    2nd view added.  Sends genes that the user entered to the 2nd view, although it only sets it in a text view for now.

--- app/src/main/java/com/archerdx/fusionlibrary/MainActivity.java
+++ app/src/main/java/com/archerdx/fusionlibrary/MainActivity.java
@@ -2,11 +2,12 @@ package com.archerdx.fusionlibrary;
 
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
-
+import android.widget.EditText;
 
 
 public class MainActivity extends AppCompatActivity {
@@ -15,6 +16,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button fusionSearchButton = (Button)findViewById(R.id.fusionSearchButton);
+
+        //Sends the fusion entered to the FusionList.java class upon clicking the Find Fusions button
+        fusionSearchButton.setOnClickListener(
+            new Button.OnClickListener(){
+                public void onClick(View v){
+                    Intent goToFusionList = new Intent(getApplication(), FusionList.class);
+
+                    final EditText genesEntered = (EditText) findViewById(R.id.genesEntered);
+                    String list_of_genes = genesEntered.getText().toString();
+
+                    goToFusionList.putExtra("genes", list_of_genes);
+                    startActivity(goToFusionList);
+
+                }
+            }
+        );
     }
 
 
